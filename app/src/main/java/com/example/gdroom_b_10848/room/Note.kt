7d0fcffac33105326961447a0de156aa27f16c41
Kevinboro25 <kevinboro25@gmail.com>
2022-09-22 08:51:18 +0700

    GDRoom_B_10848

---
+++ app/src/main/java/com/example/gdroom_b_10848/room/Note.kt
@@ -0,0 +1,12 @@
+package com.example.gd_room_b_10848.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
